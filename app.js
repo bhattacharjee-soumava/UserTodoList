f8308af31de157c030f6389f488b388ab60fabaf
Soumava Bhattacharjee <bhattacharjee.soumava@gmail.com>
2023-01-11 09:54:27 -0600

    Inital Commit

--- app.js
+++ app.js
@@ -1,182 +1,207 @@
+//jshint esversion:6
+
 const express = require("express");
+const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const app = express();
 const _ = require("lodash");
-
-//import mongoose from 'mongoose'
 const mongoose = require("mongoose");
-//mongoose.connect("mongodb://localhost:27017/todolistDB?maxPoolSize=20&w=majority");
-//mongoose.connect("mongodb+srv://soumava-admin:Test123@cluster0.kdb349w.mongodb.net/?retryWrites=true&w=majority/todolistDB");
+//Authentication:
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+const FacebookStrategy = require('passport-facebook');
 
-mongoose.connect("mongodb+srv://soumava-admin:Test123@cluster0.kdb349w.mongodb.net/todolistDB");
-const {
-  Schema,
-  model
-} = mongoose;
+
+// const TodoDaily = model('TodoDaily', todoSchemaDaily);
+// //
+// //Schema within Schema
+// //
+// const itemSchema = {
+//   todoEntry: String
+// };
 //
-//##TO do Daily ####
+// const Item = model('Item', itemSchema);
 //
-const todoSchemaDaily = new Schema({
-  todoEntry: {
-    type: String,
-    required: [true, '***Please make a valid entry***'],
-  }
-}, {
-  collection: 'todoListsDaily'
-});
-
-const TodoDaily = model('TodoDaily', todoSchemaDaily);
+// const listSchema = {
+//   name: String,
+//   items: [itemSchema]
+// };
 //
-//Schema within Schema
-//
-const itemSchema = {
-  todoEntry: String
-};
-
-const Item = model('Item', itemSchema);
-
-const listSchema = {
-  name: String,
-  items: [itemSchema]
-};
-
-const List = model('List', listSchema);
+// const List = model('List', listSchema);
 //
 //
 //
 app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 app.use(express.static('public'));
 
-//
-// let day = d.toLocaleDateString('en-US', options);
-let day = date.getDay();
+app.set('trust proxy', 1);
+app.use(session({
+  secret: 'soumavas node secret key',
+  resave: false,
+  saveUninitialized: false
 
+}));
 
-app.get("/:customListName", function(req, res) {
+app.use(passport.initialize());
+app.use(passport.session());
 
-  var customListName = _.capitalize(req.params.customListName);
-  console.log(customListName);
-  List.findOne({
-    name: customListName
-  }, function(err, foundList) {
-    console.log(foundList);
-    if (!foundList) {
-      res.render("list", {
-        listTitle: customListName,
-        newItems: []
-      });
-    } else {
-      console.log(foundList.items);
-
-      res.render("list", {
-        listTitle: customListName,
-        newItems: foundList.items
-      });
-    }
-  });
+mongoose.connect("mongodb+srv://soumava-admin:Test123@cluster0.kdb349w.mongodb.net/userTodoListDB");
+const {
+  Schema,
+  model
+} = mongoose;
+// //
+// //##TO do####
+// //
+const todoSchema = new Schema({
+  user: String,
+  todoEntry: {
+    type: String,
+    required: [true, '***Please make a valid entry***'],
+  }
+}, {
+  collection: 'todoLists'
 });
+// let day = date.getDay();
+const currentYear = new Date().getFullYear()
 
-
+// app.get("/:customListName", function(req, res) {
+//
+//   var customListName = _.capitalize(req.params.customListName);
+//   console.log(customListName);
+//   List.findOne({
+//     name: customListName
+//   }, function(err, foundList) {
+//     console.log(foundList);
+//     if (!foundList) {
+//       res.render("list", {
+//         listTitle: customListName,
+//         newItems: []
+//       });
+//     } else {
+//       console.log(foundList.items);
+//
+//       res.render("list", {
+//         listTitle: customListName,
+//         newItems: foundList.items
+//       });
+//     }
+//   });
+// });
 
 app.get("/", function(req, res) {
-
-  Item.find(function(err, todoAll) {
-    if (err) {
-      console.log(err);
-    } else {
-
-      res.render("list", {
-        listTitle: "Today",
-        newItems: todoAll
-      });
-    }
-  });
+  res.render("signin", {currentYear: currentYear});
 });
 
+app.post("/",function(req, res){res.send("<h1>Hello</h1>");});
 
-app.post("/", function(req, res) {
-
-  console.log(req.body);
-  console.log(req.body.getToDo);
-
-  const listName = req.body.list;
-  const item = new Item({
-    todoEntry: req.body.getToDo
-  });
-
-  if (req.body.list != "Today") {
-    console.log(item);
-    console.log(listName);
-
-    List.findOne({name: listName}, function(err, findPostList){
-      if (!err){
-        if (!findPostList){
-          const list = new List({
-            name: listName,
-            items: [item]
-          });
-          list.save();
-          res.render("list", {
-            listTitle: listName,
-            newItems: [item]
-          });
-        }else{
-          findPostList.items.push(item);
-          findPostList.save();
-          res.redirect("/" + listName);
-        }
-      }else{
-        console.log(err);
-      }
-
-    });
+app.post("/register",function(req, res){res.render("register");});
+// app.post("/", function(req, res) {
+//   console.log(req.body);
+//   });
+// app.get("/", function(req, res) {
+//
+//   Item.find(function(err, todoAll) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//
+//       res.render("list", {
+//         listTitle: "Today",
+//         newItems: todoAll
+//       });
+//     }
+//   });
+// });
 
 
-  } else {
+// app.post("/", function(req, res) {
+//
+//   console.log(req.body);
+//   console.log(req.body.getToDo);
+//
+//   const listName = req.body.list;
+//   const item = new Item({
+//     todoEntry: req.body.getToDo
+//   });
+//
+//   if (req.body.list != "Today") {
+//     console.log(item);
+//     console.log(listName);
+//
+//     List.findOne({name: listName}, function(err, findPostList){
+//       if (!err){
+//         if (!findPostList){
+//           const list = new List({
+//             name: listName,
+//             items: [item]
+//           });
+//           list.save();
+//           res.render("list", {
+//             listTitle: listName,
+//             newItems: [item]
+//           });
+//         }else{
+//           findPostList.items.push(item);
+//           findPostList.save();
+//           res.redirect("/" + listName);
+//         }
+//       }else{
+//         console.log(err);
+//       }
+//
+//     });
+//
+//
+//   } else {
+//
+//     //await todoDaily.save();
+//     item.save();
+//     res.redirect("/");
+//   }
+//
+// });
 
-    //await todoDaily.save();
-    item.save();
-    res.redirect("/");
-  }
-
-});
-
-app.post("/delete", function(req, res) {
-
-  const page = req.body.page;
-  const delId = req.body.myCheckbox;
-  console.log(req.body.page);
-  if (req.body.page != "Today") {
-
-
-    List.findOneAndUpdate({
-      name: page
-    }, {
-      $pull: {
-        items: {
-          _id: delId
-        }
-      }
-    }, function(err, foundList) {
-
-      if (!err) {
-        res.redirect("/" + page);
-      }
-    });
-  } else {
-    Item.findByIdAndRemove(delId, function(err) {
-      if (!err) {
-        console.log("Successfully removed document from DB.");
-        res.redirect("/");
-      }
-    });
-  }
-});
+// app.post("/delete", function(req, res) {
+//
+//   const page = req.body.page;
+//   const delId = req.body.myCheckbox;
+//   console.log(req.body.page);
+//   if (req.body.page != "Today") {
+//
+//
+//     List.findOneAndUpdate({
+//       name: page
+//     }, {
+//       $pull: {
+//         items: {
+//           _id: delId
+//         }
+//       }
+//     }, function(err, foundList) {
+//
+//       if (!err) {
+//         res.redirect("/" + page);
+//       }
+//     });
+//   } else {
+//     Item.findByIdAndRemove(delId, function(err) {
+//       if (!err) {
+//         console.log("Successfully removed document from DB.");
+//         res.redirect("/");
+//       }
+//     });
+//   }
+// });
 
 let port = process.env.PORT;
 if (port == null || port == "") {
